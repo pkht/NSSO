e4d54e24b47e91b6569e3aa5a0f23d3601806f06
Phil Thomas <phil.thomas@pkht.co.uk>
2010-10-31 18:18:51 +0000

    adding 404 page

---
+++ wp-content/themes/nsso/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div id="content">
+
+    <h2>Oops!</h2>
+
+    <p>Apologies, but the page you requested could not be found.</p>
+
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
